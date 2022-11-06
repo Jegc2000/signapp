bcbb4f963ff96a956e8c02ebb0413511a341adea
Jegc2000 <62514096+Jegc2000@users.noreply.github.com>
2022-11-05 21:52:34 -0600

    Add files via upload

--- lib/lectionspage.dart
+++ lib/lectionspage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'items.dart';
+import 'package:signapp/video2.dart';
 
 class LectionsPage extends StatefulWidget {
   const LectionsPage({super.key});
@@ -10,72 +9,200 @@ class LectionsPage extends StatefulWidget {
 }
 
 class _LectionsPageState extends State<LectionsPage> {
-  final _lectionsElements = [
-    {
-      "title": "A",
-      "description": "Letra A",
-      "image":
-          "https://i.pinimg.com/550x/15/35/4c/15354cf0b58abb4b7f1047efeb240159.jpg",
-    },
-    {
-      "title": "B",
-      "description": "Letra B",
-      "image":
-          "https://i.pinimg.com/originals/df/ab/12/dfab12165ca599ba653bc97b4da2255f.png",
-    },
-    {
-      "title": "C",
-      "description": "Letra C",
-      "image":
-          "https://2.bp.blogspot.com/-ChlvAvZWEjE/WIaTzNxvrKI/AAAAAAAAHGY/oIVQkcpS5D8MCg8LSgp1ODD5pJGBdoX2wCLcB/s1600/Captura%2Bde%2Bpantalla%2B2017-01-17%2B12.18.00.png",
-    },
-    {
-      "title": "D",
-      "description": "Letra D",
-      "image":
-          "https://1.bp.blogspot.com/-iBosj779bJ4/WIaTzdEOaYI/AAAAAAAAHGg/FvR_rNYPJiMZJSOS8c9IKbnrYUtgExs5ACLcB/s1600/Captura%2Bde%2Bpantalla%2B2017-01-17%2B12.18.01.png",
-    },
-    {
-      "title": "E",
-      "description": "Letra E",
-      "image":
-          "https://i.pinimg.com/550x/15/35/4c/15354cf0b58abb4b7f1047efeb240159.jpg",
-    },
-    {
-      "title": "F",
-      "description": "Letra F",
-      "image":
-          "https://i.pinimg.com/originals/df/ab/12/dfab12165ca599ba653bc97b4da2255f.png",
-    },
-    {
-      "title": "G",
-      "description": "Letra G",
-      "image":
-          "https://2.bp.blogspot.com/-ChlvAvZWEjE/WIaTzNxvrKI/AAAAAAAAHGY/oIVQkcpS5D8MCg8LSgp1ODD5pJGBdoX2wCLcB/s1600/Captura%2Bde%2Bpantalla%2B2017-01-17%2B12.18.00.png",
-    },
-    {
-      "title": "H",
-      "description": "Letra H",
-      "image":
-          "https://1.bp.blogspot.com/-iBosj779bJ4/WIaTzdEOaYI/AAAAAAAAHGg/FvR_rNYPJiMZJSOS8c9IKbnrYUtgExs5ACLcB/s1600/Captura%2Bde%2Bpantalla%2B2017-01-17%2B12.18.01.png",
-    },
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Lections"),
       ),
-      body: Container(
-        padding: EdgeInsets.all(12.0),
-        child: GridView.builder(
-          itemCount: _lectionsElements.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
-          itemBuilder: (BuildContext context, int index) {
-            return Items(itemsData: _lectionsElements[index]);
-          },
-        ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          ExpansionTile(
+            title: Text("Nivel Basico"),
+            children: [
+              ExpansionTile(
+                title: Text("Leccion 1: El abecedario"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/SXNJCma9MEc",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 2: Dias de la semana y meses"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/Qt7dGwpRPgo",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 3: Relaciones Familiares"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/NWEJI39YaKA",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 4: Numeros y tiempos"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/K8mXU1Q7o7A",
+                  ),
+                ],
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: Text("Nivel Medio"),
+            children: [
+              ExpansionTile(
+                title: Text("Leccion 5: Vocabulario Comun"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/d74Y5sE0868",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 6: Cuestionamientos"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/yinh2hENVpY",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 7: Colores"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/U7m4daxkSBQ",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text(
+                    "Leccion 8: Sustantivos, adjetivos, sentimientos y estados de ánimo"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/nKGH3mJxRcs",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 9: Verbos narrativos"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/ZpsRJUCkUO0",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 10: Verbos comunes"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/USQOKJSFOrE",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 11: Adverbios de tiempo"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/CwHSLLMMe2o",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 12: Grupo de señas"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/hBdnKn6tmx4",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 13: Ciudades y estados"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/D4D7xJyLZB0",
+                  ),
+                ],
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: Text("Nivel Avanzado"),
+            children: [
+              ExpansionTile(
+                title: Text("Leccion 14: Vocabulario escolar"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/Md9EaeQ9KIY",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 15: Vocabulario comun medico"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/wEtVaJd1SHk",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 16: Vocabulario medico avanzado"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/DSpeIpOVrlg",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 17: Oficios y profeciones"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/zjNNVRsMhVI",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 18: Vocabulario legal"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/P7FGpKtlzzI",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 19: Vocabulario religioso"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/Ilc8IWsLgas",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 20: Vocabulario comun en hoteles"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/4EncGFVWI5U",
+                  ),
+                ],
+              ),
+              ExpansionTile(
+                title: Text("Leccion 21: Vocabulario de cine"),
+                children: [
+                  videoplayer(
+                    video: "https://youtu.be/mqvu-fvdwac",
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
